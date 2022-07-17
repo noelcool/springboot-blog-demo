b33ffe8436f66ff3203bbbdf4ac192615120304d
noel <noelcoolblue@gmail.com>
2022-07-17 18:22:34 +0900

    :tada: Begin a project
    Begin a project

---
+++ noelog/src/main/java/com/noelog/NoelogApplication.java
@@ -0,0 +1,13 @@
+package com.noelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoelogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoelogApplication.class, args);
+    }
+
+}
---
+++ noelog/src/test/java/com/noelog/NoelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoelogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
