2fd83f9d57c51c5f2d85ed5d0afb7d1008aaa051
noel <noelcoolblue@gmail.com>
2022-07-22 15:13:22 +0900

    :sparkles: PostController Service에서 저장하도록 수정

--- noelog/src/main/java/com/noelog/api/controller/PostController.java
+++ noelog/src/main/java/com/noelog/api/controller/PostController.java
@@ -1,6 +1,8 @@
 package com.noelog.api.controller;
 
 import com.noelog.api.domain.value.PostValue;
+import com.noelog.api.service.PostService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,14 +13,18 @@ import java.util.Map;
 
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
+
+    private final PostService postService;
 
     // ssr : html rendering
     // spa : javascript <-> api
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostValue.Req.Creation creation) {
-        log.info("params = {}, {}", creation.title(), creation.content());
+    public Map<String, String> post(@RequestBody @Valid PostValue.Req.Creation request) {
+        log.info("params = {}, {}", request.title(), request.content());
+        postService.write(request);
         return Map.of();
     }
 
