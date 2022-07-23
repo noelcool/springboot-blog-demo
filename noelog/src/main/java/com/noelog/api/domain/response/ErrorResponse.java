72e5de7bf7978045c76fb39bc7ab37444e5ac249
noel <noelcoolblue@gmail.com>
2022-07-23 22:36:50 +0900

    :sparkles: Add builder pattern

--- noelog/src/main/java/com/noelog/api/domain/response/ErrorResponse.java
+++ noelog/src/main/java/com/noelog/api/domain/response/ErrorResponse.java
@@ -1,20 +1,25 @@
 package com.noelog.api.domain.response;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Getter @Setter
-@RequiredArgsConstructor
 public class ErrorResponse {
 
     private final String code;
     private final String message;
 
     private Map<String, String> validation = new HashMap<>();
+
+    @Builder
+    public ErrorResponse(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 
     public void addValidation(String fieldName, String errorMessage) {
         this.validation.put(fieldName, errorMessage);
