bb9c0238c0d716bb6ad484fb26a104dfbc2d2b38
noel <noelcoolblue@gmail.com>
2022-07-23 22:38:20 +0900

    :memo:

--- noelog/src/main/java/com/noelog/api/controller/PostController.java
+++ noelog/src/main/java/com/noelog/api/controller/PostController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Map;
 
 @Slf4j
 @RestController
@@ -22,10 +21,12 @@ public class PostController {
     // spa : javascript <-> api
 
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostValue.Req.Creation request) {
-        log.info("params = {}, {}", request.title(), request.content());
+    public void post(@RequestBody @Valid PostValue.Req.Creation request) {
+        // 1. 저장된 데이터 entity -> response로 응답하기
+        // 2. 저장한 데이터의 primary_id -> response로 응답하기
+        // 3. 응답 필요 없음
+        // bad : 서버에서 꼭 이렇게 한다고 fix하지 말고 유연하게 대응하는 것이 좋다
         postService.write(request);
-        return Map.of();
     }
 
 
